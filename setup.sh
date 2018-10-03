b380ffddb36d8a45f88918880c49d74198b377fc
Hossam Mourad <ihos4m@gmail.com>
2018-10-03 16:33:06 +0400

    basic setup of websocket.io and create separate directories for client and server

---
+++ setup.sh
@@ -0,0 +1,10 @@
+printf ">>> Installing whole product dependencies <<<\n"
+npm i
+
+printf ">>> Installing front end dependencies <<<\n"
+cd ./client
+npm i
+
+printf ">>> Installing backend end dependencies <<<\n"
+cd ./server
+npm i
